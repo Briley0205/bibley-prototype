a2c176c350c39440f8a15113fb2c725f515b80d5
unknown <81542046+Briley0205@users.noreply.github.com>
2022-05-21 17:01:40 +0900

    7

--- src/server.js
+++ src/server.js
@@ -37,11 +37,11 @@ app.use(session({
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
 }));
-app.use(function(req, res, next) {
+app.all(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", '*');
     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Access-Control-Allow-Methods", 'GET,POST,DELETE,OPTIONS');
-    res.setHeader("Access-Control-Allow-Headers", 'Content-type');
+    res.setHeader("Access-Control-Allow-Headers", 'X-Requested-With,content-type');
     next();
 })
 app.use(cors({
