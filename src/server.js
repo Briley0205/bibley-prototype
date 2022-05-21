7d10db52e713a988684f7363cead753e56de5b04
unknown <81542046+Briley0205@users.noreply.github.com>
2022-05-21 16:33:41 +0900

    4

--- src/server.js
+++ src/server.js
@@ -40,8 +40,8 @@ app.use(session({
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Origin", 'Origin,X-Requested-With,Content-type');
+    res.header("Access-Control-Allow-Methods", 'GET,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Content-type');
     next();
 })
 app.use(cors({
