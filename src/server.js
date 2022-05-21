77911b3e013ce4a91e3c09236497e0f338c27ab4
unknown <81542046+Briley0205@users.noreply.github.com>
2022-05-21 17:11:26 +0900

    9

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.use(session({
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
 }));
-app.use(function(req, res, next) {
+app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", 'GET,POST,DELETE,OPTIONS');
