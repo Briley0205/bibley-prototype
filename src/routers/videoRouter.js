982c3531f2d3e7f00587f66f81ff5e354724b151
unknown <81542046+Briley0205@users.noreply.github.com>
2022-05-21 17:17:52 +0900

    9

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,13 @@ import { getVideoUpload, postVideoUpload, Watch, getEdit, postEdit, deleteVideo 
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id([0-9a-f]{24})", Watch);
+videoRouter.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", 'GET,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+}).get("/:id([0-9a-f]{24})", Watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").all(protectMiddleware).get(getEdit).post(videoUpload.fields([
     { name: "video", maxCount: 1 }, { name: "thumb", maxCount: 1 }, { name: "captions", maxCount: 1 },
 ]), postEdit);
