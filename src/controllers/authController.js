9ec294e31a17523e29541cda7e2efe9844d31687
unknown <81542046+Briley0205@users.noreply.github.com>
2022-05-27 12:51:37 +0900

    twitter login 2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -218,11 +218,64 @@ export const startTwitterLogin = (req, res) => {
     client_secret: process.env.TT_SECRET,
     redirect_uri: "https://bibley.herokuapp.com/auth/twitter/finish",
     response_type: "code",
-    include_granted_scopes: true,
+    state: "abc",
     scope: "users.read offline.access",
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
-export const finishTwitterLogin = (req, res) => {};
+export const finishTwitterLogin = (req, res) => {
+    const baseUrl = "https://api.twitter.com/2/oauth2/token";
+  const config = {
+    client_id: process.env.GG_CLIENT,
+    client_secret: process.env.GG_SECRET,
+    redirect_uri: "https://bibley.herokuapp.com/auth/google/finish",
+    include_granted_scopes: true,
+    grant_type: "authorization_code",
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://www.googleapis.com";
+    const userData = await (
+      await fetch(`${apiUrl}/oauth2/v1/userinfo`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    if (userData.verified_email === false) {
+      res.render("social-login", {
+        errorMessage: "Can't access your email information.",
+      });
+      res.redirect("/login");
+    }
+    let user = await User.findOne({ email: userData.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.picture,
+        username: userData.name,
+        email: userData.email,
+        password: "",
+        socialOnly: true,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    req.flash("info", "Welcome!");
+    return res.redirect("/");
+  } else {
+    return res.redirect("/auth/social_login");
+  }
+};
